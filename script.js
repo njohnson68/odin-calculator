c3c146b4b1f15b9b3436341e4680a091bc9d709a
Nicholas Johnson <nicholas8765@sbcglobal.net>
2024-07-05 17:33:07 -0500

    calculator now evaluates one pair of numbers at a time, also added in styling

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-let number1;
-let operator;
-let number2;
+let number1 = "";
+let operator = "";
+let number2 = "";
 
 function add(num1, num2) {
-  console.log(num1 + num2);
+  display.value = parseFloat(num1) + parseFloat(num2);
 }
 
 function subtract(num1, num2) {
-  console.log(num1 - num2);
+  display.value = num1 - num2;
 }
 
 function multiply(num1, num2) {
-  console.log(num1 * num2);
+  display.value = num1 * num2;
 }
 
 function divide(num1, num2) {
-  console.log(num1 / num2);
+  display.value = num1 / num2;
 }
 
 function operate(num1, op, num2) {
@@ -38,10 +38,41 @@ const display = document.querySelector(".display");
 numButtons.forEach((button) => {
   button.addEventListener("click", () => {
     display.value += button.textContent;
+    if (operator == "") {
+      number1 += button.textContent;
+    } else {
+      number2 += button.textContent;
+    }
   });
 });
 
 const clear = document.querySelector(".clear-btn");
 clear.addEventListener("click", () => {
   display.value = "";
+  number1 = "";
+  operator = "";
+  number2 = "";
+});
+
+const opButtons = document.querySelectorAll(".op-btn");
+opButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (number1 !== "" && number2 !== "" && operator !== "") {
+      operate(number1, operator, number2);
+      number1 = display.value;
+      number2 = "";
+    }
+    if (number1 !== "" && number2 == "" && operator !== "") {
+      display.value.replace(/.$/, "");
+    }
+    display.value += button.textContent;
+    operator = button.textContent;
+  });
+});
+
+const equals = document.querySelector(".eq-btn");
+equals.addEventListener("click", () => {
+  if (number1 !== "" && number2 !== "" && operator !== "") {
+    operate(number1, operator, number2);
+  }
 });
